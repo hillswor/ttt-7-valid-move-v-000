a15e9166c866a0c84d29abd0a1117b77177c4fef
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-08-20 21:13:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ def position_taken? (board, index)
  def valid_move? (board, index)
    move = index.to_i
    position = move - 1
-   if position_taken?(board, position) == false || position_taken?(board, position) = nil && position.between?(0, 8)
+   if position_taken?(board, position) == false && position.between?(0, 8)
      true
    elsif position_taken?(board, position) == true && position.between?(0, 8)
      false
